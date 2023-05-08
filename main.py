51977d74b03e8370b67c46e3286c0f22ead93e8d
yyt-gw <yeyintthu@globalwalkers.co.jp>
2023-05-08 16:59:25 +0630

    update operations

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ from enum import Enum
 import mlflow
 import os
 import pickle
+import pandas as pd
 import yaml
 import torch
 from datetime import datetime
-from typing import Iterator, List, Dict
+from typing import Iterator, List, Dict, Union
 
 # import custom packages
 from dataset.multi_label_weather_dataset import MultiLabelWeatherDataset
 from model.model import MultiLabelBinaryClassifier
-from utils.utils import train, evaluate, get_transforms
+from utils.utils import train_model, evaluate_model, validate_model, get_transforms
 
 
 # define dataset types
@@ -27,7 +28,16 @@ class OperationTypes(Enum):
     TRAIN_EVAL = "train_eval"
 
 
-class MLFlowTracker:
+class MLFlowExperiment:
+    """MLFLowExperiment contains functions to log output of training
+    as mlflow metrics and artifacts
+
+    Args:
+        exp_name (str): Name of mlflow experiment
+        config (Dict): Config params for logging
+        log_dir (str): Directory to save some mlflow artifacts of the experiment
+    """
+
     def __init__(
         self, exp_name: str, config: Dict, log_dir: str = "mlflow_logs"
     ) -> None:
@@ -41,28 +51,37 @@ class MLFlowTracker:
             self.experiment_id = mlflow.create_experiment(exp_name)
 
     def init_run(self):
+        """Start new MLFlow run"""
         mlflow.start_run(
             run_name=datetime.now().strftime("%H_%M_%S_%d_%M_%Y"),
             experiment_id=self.experiment_id,
         )
 
     def terminate_run(self):
+        """End active MLFlow run"""
         mlflow.end_run()
 
     def log_params(self):
+        """Log a batch of training params for the current run"""
         mlflow.log_params(
             {
-                "Batch size": config["batch_size"],
+                "Batch size": config["dataset"]["batch_size"],
                 "Backbone type": config["backbone"]["type"],
                 "Frozen backbone layers": config["backbone"]["frozen_layers"],
-                "Input size": config["input_size"],
+                "Input size": config["backbone"]["input_size"],
                 "Learning rate": config["training"]["lr"],
-                "Number of epochs": config["epochs"],
-                "Milestones": config["milestones"],
+                "Number of epochs": config["training"]["epochs"],
+                "Milestones": config["training"]["milestones"],
             }
         )
 
     def log_config(self, config_path: str = "config.yml"):
+        """Log config file as MLFLow artifact
+
+        Args:
+            config_path (str, optional): Path of config file which is used in training.
+                Defaults to "config.yml".
+        """
         os.makedirs(self.log_dir, exist_ok=True)
         source_config_path = os.path.join(self.log_dir, config_path)
         with open(source_config_path, "w") as con_f:
@@ -70,17 +89,45 @@ class MLFlowTracker:
         mlflow.log_artifact(source_config_path, artifact_path=config_path)
 
     def log_losses(self, losses: Iterator):
-        # start mlflow run
+        """Log BCE losses of current run
+
+        Args:
+            losses (Iterator): _description_
+        """
         for i, loss_info in enumerate(losses):
             mlflow.log_metrics(
                 {"BCE train loss": loss_info[0], "BCE val Loss": loss_info[1]},
                 i,
             )
 
-    def log_model(self, model):
+    def log_model(self, model: torch.nn.Module):
+        """Log model of current run
+
+        Args:
+            model (torch.nn.Module): Pytorch model
+        """
         mlflow.pytorch.log_model(
-            model, artifact_path="best-model", pickle_module=pickle
+            model,
+            artifact_path="best-model",
+            pickle_module=pickle,
+            pip_requirements=["torch==1.13.1"],
         )
+
+
+def print_gitstyle_classification_report(report_dict: Dict) -> None:
+    """Format classification report as github style table and print it out
+    along with  overall accuracy
+
+    Args:
+        report_dict(Dict): Dictionayry of metrics of a classification report    
+    """
+    accuracy = report_dict.pop('accuracy')
+    report_df = pd.DataFrame(report_dict).transpose()
+    print(
+        f"\n\nClassification report for {class_} weather classification model:"
+        + f"\n\n{report_df.to_markdown(index=True,tablefmt='github')}\n\n"
+        + f"  Overall accuracy : {accuracy}\n\n"
+    )
 
 
 def get_dataloader(
@@ -121,11 +168,13 @@ def get_dataloader(
     )
 
 
-def operate_training(
+def train(
     model: torch.nn.Module,
     config: Dict,
     device: torch.device,
-    mlflow_tracker: MLFlowTracker,
+    mlflow_tracker: MLFlowExperiment,
+    eval: bool,
+    output_dir: str = "outputs/default_exp/",
 ):
     """Train the model with provided configuration params
 
@@ -134,8 +183,11 @@ def operate_training(
             binary classifier
         config (Dict): Dict of config params
         device (torch.device): Device on which training will be run
-        mlflow_tracker (MLFlowTracker): Mlflow tracker for logging metrics,
+        mlflow_tracker (MLFlowExperiment): Mlflow tracker for logging metrics,
             params and artifacts
+        eval (bool): Operate evalution or not
+        output_dir (str, optional): directory to save outputs from trianing process.
+            Default to `outputs/default_exp/`
     """
     # create transformation pipelines
     input_size = config["backbone"]["input_size"]
@@ -148,11 +200,10 @@ def operate_training(
     train_loader = get_dataloader(config, classes, train_transforms)
     val_loader = get_dataloader(config, classes, val_transforms, for_train=False)
 
-    exp_ext = datetime.now().strftime("%H_%M_%S_%d_%M_%Y")
-    weights_dir = os.path.join(*[config["saved_dir"], "weights", f"Exp_at_{exp_ext}"])
+    weights_dir = os.path.join(output_dir, "weights")
 
     # training
-    best_model, losses = train(
+    best_weights, losses = train_model(
         model,
         device,
         config["training"]["lr"],
@@ -162,18 +213,41 @@ def operate_training(
         config["training"]["milestones"],
         weights_dir,
     )
+    model.load_state_dict(best_weights)
+
     # write training and validation losses to tensorboard
     mlflow_tracker.init_run()
     mlflow_tracker.log_losses(losses)
     mlflow_tracker.log_config()
     mlflow_tracker.log_params()
-    mlflow_tracker.log_model(model=best_model)
+    mlflow_tracker.log_model(model)
     mlflow_tracker.terminate_run()
+    if eval:
+        eval_reports_dict = evaluate(model, config, device, classes)
+        for _, eval_report in eval_reports_dict.items():  # type: ignore
+            print_gitstyle_classification_report(eval_report)
 
 
-def operate_evaluation(
-    model: torch.nn.Module, config: Dict, device: torch.device, classes: List
-):
+def evaluate(
+    model: torch.nn.Module,
+    config: Dict,
+    device: torch.device,
+    classes: List,
+) -> Dict[str, Dict[str, Union[Dict, float]]]:
+    """Evaluate model and generate classification reports for the validation set
+
+    Args:
+        model (torch.nn.Module): Nn module representation of multi-label
+            binary classifier
+        config (Dict): Dict of config params
+        device (torch.device): Device on which evaluation will be run
+        classes (List): List of classes of the dataset
+
+    Returns:
+        Dict[str, Dict[str, Union[Dict, float]]]: Dictionary of dictionaries
+            for classification reports of classifiers
+
+    """
     # create transformation pipelines
     input_size = config["backbone"]["input_size"]
     mean = config["dataset"]["mean"]
@@ -181,9 +255,8 @@ def operate_evaluation(
     val_transforms = get_transforms(input_size, mean, std, train_pipeline=False)
     # create datasets and dataloaders for training and validation sets
     val_loader = get_dataloader(config, classes, val_transforms, for_train=False)
-    classification_reports_dict = evaluate(model, device, val_loader, classes)
-    for _, classification_report in classification_reports_dict.items():
-        print(classification_report)
+    classification_reports = evaluate_model(model, device, val_loader, classes)
+    return classification_reports
 
 
 if __name__ == "__main__":
@@ -225,12 +298,29 @@ if __name__ == "__main__":
     classifier = MultiLabelBinaryClassifier(
         classes, config["backbone"]["type"], config["backbone"]["frozen_layers"], device
     )
+    exp_ext = datetime.now().strftime("%H_%M_%S_%d_%M_%Y")
+    output_dir = os.path.join(config["saved_dir"], exp_ext)
+    mlflow_log_dir = os.path.join(output_dir, "mlflow-log")
+    # run operation based on operation type
     if args.operation_type == OperationTypes.TRAIN.value:
-        mlflow_tracker = MLFlowTracker("test-exp", config)
-        operate_training(classifier, config, device, mlflow_tracker)
+        mlflow_tracker = MLFlowExperiment("test-exp", config, mlflow_log_dir)
+        train(
+            classifier,
+            config,
+            device,
+            mlflow_tracker,
+            eval=False,
+            output_dir=output_dir,
+        )
     elif args.operation_type == OperationTypes.EVALUATE.value:
-        operate_evaluation(classifier, config, device, classes)
+        classifier.load_state_dict(torch.load(config["evaluation"]["weights"]))
+        evaluation_reports = evaluate(classifier, config, device, classes)
+        for class_, eval_report in evaluation_reports.items():  # type: ignore
+            print_gitstyle_classification_report(eval_report)
     elif args.operation_type == OperationTypes.TRAIN_EVAL.value:
-        mlflow_tracker = MLFlowTracker("test-exp", config)
-        operate_training(classifier, config, device, mlflow_tracker)
-        operate_evaluation(classifier, config, device, classes)
+        mlflow_tracker = MLFlowExperiment("test-exp", config, mlflow_log_dir)
+        train(
+            classifier, config, device, mlflow_tracker, eval=True, output_dir=output_dir
+        )
+    elif args.operation_type == OperationTypes.VALIDATE.value:
+        validate_model(classifier, config, classes)
--- utils/utils.py
+++ utils/utils.py
@@ -3,13 +3,17 @@
     Email  : yeyintthu536@gmail.com
 """
 from copy import deepcopy
+import cv2
+import numpy as np
 import os
 from sklearn.metrics import classification_report
 import torch
 import torchvision
-from typing import List, Iterator, Tuple, Dict
+from typing import List, Iterator, Tuple, Dict, Union
 from random import uniform
-
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
 
 def criterion(loss_fn, preds, gts, device):
     losses = torch.zeros((len(preds.keys())), device=device)
@@ -18,7 +22,7 @@ def criterion(loss_fn, preds, gts, device):
     return torch.mean(losses)
 
 
-def train(
+def train_model(
     model: torch.nn.Module,
     device: torch.device,
     lr_rate: float,
@@ -27,7 +31,7 @@ def train(
     val_loader: torch.utils.data.DataLoader,
     milestones: List[int],
     saved_dir: str,
-) -> Tuple[torch.nn.Module, Iterator]:
+) -> Tuple[Dict, Iterator]:
     """Train the model with multi-steps learning rates for a given order of epochs
         and return the best checkpointed model, training and validation losses
 
@@ -42,7 +46,8 @@ def train(
         saved_dir (str): Directory to save model checkpoints
 
     Returns:
-        Tuple[torch.nn.Modue, Iterator]: Training and validation losses through epochs
+        Tuple[Dict, Iterator]: Best model weights and training and validation losses
+            through epochs
     """
     # define scheduled optimizer and loss func
     optimizer = torch.optim.Adam(model.parameters(), lr=lr_rate)
@@ -106,53 +111,224 @@ def train(
                             )
                     if data_loader["type"] == "train":
                         train_epoch_loss.append(current_loss)
-    model.load_state_dict(best_model_wts)
-    return model, zip(*[train_epoch_loss, validation_epoch_loss])
+    return best_model_wts, zip(*[train_epoch_loss, validation_epoch_loss])
 
 
-def evaluate(
+def evaluate_model(
     model: torch.nn.Module,
     device: torch.device,
     data_loader: torch.utils.data.DataLoader,
     classes: List,
-) -> Dict:
+) -> Dict[str, Dict[str, Union[Dict, float]]]:
     """Evaluate multi-label binary classifier and return classification reports
         for classifiers
 
     Args:
         model (torch.nn.Module): Nn module representation of multi-label
-            binary classification
+            binary classification model
         device (torch.device): Device on which evalution will be run
         data_loader (_type_): Dataloader for evalution
         classes (List): List of classes
 
     Returns:
-        Dict: Dict of classification reports for binary classifiers
+        Dict[str, Dict[str, Union[Dict, float]]]: Dict of classification report dicts
+            for classifiers' metrics
     """
+    model.to(device)
     model.eval()
-    gts, preds = {}, {}  # type: ignore
+    classes_gts, classes_preds = {}, {}  # type: ignore
     for class_ in classes:
-        gts[class_] = []
-        preds[class_] = []
-    for i, samples in enumerate(data_loader):
+        classes_gts[class_] = []
+        classes_preds[class_] = []
+    for i, samples in tqdm(enumerate(data_loader), desc="Evaluating.."):
         images = samples["image_tensors"].to(device)
         gts = samples["labels"]
         for class_, val in gts.items():
-            gts[class_] = gts[class_] + [int(label) for label in val.tolist()]
+            classes_gts[class_] = classes_gts[class_] + [
+                int(label) for label in val.tolist()
+            ]
         with torch.no_grad():
             preds = model(images)
         for class_, val in preds.items():
-            preds[class_] = preds[class_] + [round(prob[0]) for prob in val.tolist()]
+            classes_preds[class_] = classes_preds[class_] + [
+                round(prob[0]) for prob in val.cpu().numpy()
+            ]
 
     classification_reports_dict = {
         class_: classification_report(
-            gts[class_],
-            preds[class_],
+            classes_gts[class_],
+            classes_preds[class_],
             target_names=[f"Not {class_}", class_],
+            output_dict=True,
         )
         for class_ in classes
     }
     return classification_reports_dict
+
+
+def predict(
+    model: torch.nn.Module,
+    img_paths: List[str],
+    transforms: torch.nn.Module,
+    device: torch.device,
+    classes: List[str],
+) -> Dict[str, List]:
+    """Predict values from multi-labels binary classification model
+    for a given set of image paths
+
+    Args:
+        model (torch.nn.Module): Nn module representation of multi-label
+            binary classification model
+        img_paths (List[str]): Paths of images
+        transforms (torch.nn.Module): Transformation pipeline before feeding
+            into the model
+        device (torch.device): Device on which predction will be run
+        classes (List[str]): Classes of the dataset
+
+    Returns:
+        Dict[str, List]: Dict of predcitions for each of binary classifier
+    """
+    predicted_classes: Dict[str, List] = {class_: [] for class_ in classes}
+
+    for img_path in tqdm(img_paths, desc="Predicting.."):
+        img = Image.open(img_path).convert("RGB")
+        img = transforms(img)
+        img = torch.stack([img]).to(device)
+        with torch.no_grad():
+            preds = model(img)
+        for class_, pred in preds.items():
+            predicted_classes[class_].append(round(pred.tolist()[0][0]))
+    return predicted_classes
+
+
+def validate_model(model: torch.nn.Module, config: Dict, classes: List) -> None:
+    """Validate and visualize predicted results and compare with ground truth labels
+
+    Args:
+        model (torch.nn.Module): Nn module representation of multi-label
+            binary classification model
+        config (Dict): Config params
+        classes (List): Classes of the dataset
+    """
+    # create directory to save validated images
+    os.makedirs(config["validated_images_dir"], exist_ok=True)
+    # retrieve image paths, ground truth labels and classes
+    img_paths: List = []
+    with open(config["dataset"]["val_path"], "r") as f:
+        val_annos = [sample.replace("\n", "") for sample in f.readlines()]
+    with open(config["dataset"]["classes_path"], "r") as f:
+        classes = f.read().rstrip().split("\n")
+    img_paths = []
+    gts: Dict[str, List] = {class_: [] for class_ in classes}
+    for val_sample in val_annos:
+        file_path, labels = val_sample.split()[0], [
+            int(label) for label in val_sample.split()[1:]
+        ]
+        img_paths.append(os.path.join(config["dataset"]["val_images_root"], file_path))
+        for i, class_ in enumerate(classes):
+            gts[class_].append(labels[i])
+    # create transformation pipelines
+    input_size = config["backbone"]["input_size"]
+    mean = config["dataset"]["mean"]
+    std = config["dataset"]["std"]
+    transforms = get_transforms(input_size, mean, std, train_pipeline=False)
+    # load model with weights in eval mode
+    device = torch.device(config["device"] if torch.cuda.is_available() else "cpu")
+    model.load_state_dict(torch.load(config["evaluation"]["weights"]))
+    model.eval()
+    model.to(device)
+    # predict and visualize for validation
+    preds = predict(model, img_paths, transforms, device, classes)
+    print(f'Saving resut images in {config["validated_images_dir"]}..')
+    visualize_preds_vs_gts(
+        img_paths, preds, gts, classes, saved_images_dir=config["validated_images_dir"]
+    )
+    print('Done!')
+
+
+def visualize_preds_vs_gts(
+    img_paths: List[str],
+    preds: Dict[str, List],
+    gts: Dict[str, List],
+    classes: List[str],
+    saved_images_dir: str,
+) -> None:
+    """Visualize the comparison of prediction and ground truth values on images
+
+    Args:
+        img_paths (List[str]): Paths of images for validation
+        preds (Dict[str, List]): Prediction results of binary classifiers
+            for validated images
+        gts (Dict[str, List]): Ground truth values of binary classifiers
+            for validated images
+        classes (List[str]): Classes of dataset
+        saved_images_dir (str): Director to save validated images
+    """
+    for i, img_path in enumerate(img_paths):
+        img = cv2.imread(img_path)
+        img_2 = np.ones((600, 600, 3), dtype=np.uint8) * 255
+        txt_x = 90
+        cv2.putText(
+            img_2,
+            "Gt",
+            (20, 40),
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            1,
+            (0, 0, 0),
+            1,
+        )
+        cv2.putText(
+            img_2,
+            "Pred",
+            (20, 60),
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            1,
+            (0, 0, 0),
+            1,
+        )
+        for j, class_ in enumerate(classes):
+            txt_y = 20
+            label_color = (
+                (0, 255, 0) if gts[class_][i] == preds[class_][i] else (0, 0, 255)
+            )
+            cv2.putText(
+                img_2,
+                class_,
+                (txt_x, txt_y),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                1,
+                (0, 0, 0),
+                1,
+            )
+            txt_y += 20
+            cv2.putText(
+                img_2,
+                "yes" if gts[class_][i] else "no",
+                (txt_x, txt_y),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                1,
+                (0, 255, 0),
+                1,
+            )
+            txt_y += 20
+            cv2.putText(
+                img_2,
+                "yes" if preds[class_][i] else "no",
+                (txt_x, txt_y),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                1,
+                label_color,
+                1,
+            )
+            txt_x += 90
+
+        x_offset = y_offset = 150
+        img_2[
+            y_offset : y_offset + img.shape[0], x_offset : x_offset + img.shape[1]
+        ] = img
+        cv2.imwrite(
+            os.path.join(saved_images_dir, f"validated_{Path(img_path).name}"), img_2
+        )
 
 
 def get_transforms(
@@ -204,3 +380,5 @@ def get_transforms(
     else:
         transform_pipeline = transforms
     return transform_pipeline
+
+
